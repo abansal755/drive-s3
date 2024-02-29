6e91670388da9cf63a87ffd523235fcbdb66661e
Akshit Bansal <akshit.bansal755@gmail.com>
2024-02-29 13:50:12 +0530

    modified root folder caching

--- ui/src/components/RootIndex.jsx
+++ ui/src/components/RootIndex.jsx
@@ -2,18 +2,20 @@ import { useQuery } from "@tanstack/react-query";
 import { Navigate } from "react-router-dom";
 import { apiInstance } from "../lib/axios";
 import AuthContextLoading from "./common/AuthContextLoading";
+import { useAuthContext } from "../context/AuthContext.jsx";
 
 const RootIndex = () => {
-    const { data: rootFolder, isSuccess } = useQuery({
-        queryKey: ['rootFolder'],
-        queryFn: async () => {
-            const { data } = await apiInstance.get('/api/v1/folders/root');
-            return data;
-        },
-        gcTime: 0
-    });
-    if(isSuccess) return <Navigate to={`../folder/${rootFolder.id}`}/>;
-    return <AuthContextLoading/>;
-}
+  const { user } = useAuthContext();
+  const { data: rootFolder, isSuccess } = useQuery({
+    queryKey: ["user", user.id, "rootFolder"],
+    queryFn: async () => {
+      const { data } = await apiInstance.get("/api/v1/folders/root");
+      return data;
+    },
+    staleTime: Infinity,
+  });
+  if (isSuccess) return <Navigate to={`../folder/${rootFolder.id}`} />;
+  return <AuthContextLoading />;
+};
 
 export default RootIndex;

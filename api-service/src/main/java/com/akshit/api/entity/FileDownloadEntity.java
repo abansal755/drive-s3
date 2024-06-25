012d7eb17245d6abce7078823b1529b694eb97d0
Akshit Bansal <bansalakshitwork@gmail.com>
2024-06-26 00:24:39 +0530

    made fields volatile

--- api-service/src/main/java/com/akshit/api/entity/FileDownloadEntity.java
+++ api-service/src/main/java/com/akshit/api/entity/FileDownloadEntity.java
@@ -1,18 +1,15 @@
 package com.akshit.api.entity;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class FileDownloadEntity {
-    private String id;
-    private Long userId;
-    private Long fileId;
-    private DownloadStatus status;
+    private final String id;
+    private final Long userId;
+    private final Long fileId;
+    private volatile DownloadStatus status;
 }
--- api-service/src/main/java/com/akshit/api/entity/FileUploadEntity.java
+++ api-service/src/main/java/com/akshit/api/entity/FileUploadEntity.java
@@ -1,15 +1,13 @@
 package com.akshit.api.entity;
 
-import jakarta.persistence.*;
 import lombok.*;
 
 @Data
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class FileUploadEntity {
-    private String id;
-    private Long fileId;
-    private Long userId;
-    private UploadStatus uploadStatus;
+    private final String id;
+    private final Long fileId;
+    private final Long userId;
+    private volatile UploadStatus uploadStatus;
 }
